e4fd8d1b5413ec744fafa1bd3fa8c2854e36869c
Firoozi-S <S.Firoozi.1998@gmail.com>
2024-08-06 23:55:29 -0700

    Setting up dataset import

---
+++ main.py
@@ -0,0 +1,20 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+# Define a transform to normalize the data
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# Download and load the training data
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True, num_workers=2)
+
+# Download and load the test data
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+print(f"Number of training samples: {len(trainset)}")
+print(f"Number of test samples: {len(testset)}")
